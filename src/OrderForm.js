20f05a68e02a2fc368d92a98b233a5b4cf75b957
Doug Stoker <dougstoker512@gmail.com>
2019-06-13 13:34:27 -0600

    commit

--- src/OrderForm.js
+++ src/OrderForm.js
@@ -20,6 +20,7 @@ class OrderForm extends React.Component{
 
     handleChange = (event) => {
         this.setState({value: event.target.value});
+        console.log(this.state)
     }
 
     handleSubmit = (event) => {
@@ -31,67 +32,67 @@ class OrderForm extends React.Component{
             <div className='orderForm'>
                 <form id='orderForm'>
                     
-                    <label for='orderName'>
+                    <label htmlFor='orderName'>
                         name
                         <input name='orderName' type='text' onChange={this.handleChange} value={this.state.name}></input>
                     </label>
 
                     
-                    <label for='orderPayOnPickup'>
+                    <label htmlFor='orderPayOnPickup'>
                         pay at store
                         <input name='orderPayOnPickup' type='radio' onChange={this.handleChange} value={this.state.payOnPickup}></input>
                     </label>
 
                     
-                    <label for='orderCard'>
+                    <label htmlFor='orderCard'>
                         card number
                         <input name='orderCard' type='number' onChange={this.handleChange} value={this.state.cardNumber}></input>
                     </label>
 
                     
-                    <label for='orderCVV'>
+                    <label htmlFor='orderCVV'>
                         security code
                         <input name='orderCVV' type='number' onChange={this.handleChange} value={this.state.cvv}></input>
                     </label>
 
                     
-                    <label for='orderExpiryDate'>
+                    <label htmlFor='orderExpiryDate'>
                         expiration date
                         <input name='orderExpiryDate' type='month' onChange={this.handleChange} value={this.state.expiryDate}></input>
                     </label>
 
                     
-                    <label for='orderAddrLn1'>
+                    <label htmlFor='orderAddrLn1'>
                         address line 1
                         <input name='orderAddrLn1' type='text' onChange={this.handleChange} value={this.state.addr1}></input>
                     </label>
 
                     
-                    <label for='orderAddrLn2'>
+                    <label htmlFor='orderAddrLn2'>
                         address line 2
                         <input name='orderAddrLn2' type='text' onChange={this.handleChange} value={this.state.addr2}></input>
                     </label>
 
                     
-                    <label for='orderCity'>
+                    <label htmlFor='orderCity'>
                         city
                         <input name='orderCity' type='text' onChange={this.handleChange} value={this.state.city}></input>
                     </label>
 
                     
-                    <label for='orderState'>
+                    <label htmlFor='orderState'>
                         state
                         <input name='orderState' type='text' onChange={this.handleChange} value={this.state.state}></input>
                     </label>
 
                     
-                    <label for='orderZip'>
+                    <label htmlFor='orderZip'>
                         zip
                         <input name='orderZip' type='number' onChange={this.handleChange} value={this.state.zip}></input>
                     </label>
 
                     
-                    <label for='orderPhone'>
+                    <label htmlFor='orderPhone'>
                         phone
                         <input name='orderPhone' type='tel' onChange={this.handleChange} value={this.state.phone}></input>
                     </label>
