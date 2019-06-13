dabd68efc0d585cd2f52c564b2f6265bd9e8ee6c
Doug Stoker <dougstoker512@gmail.com>
2019-06-13 10:35:46 -0600

    added order form js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 // Load in other views here
-import Specials from './Specials';
+//import Specials from './Specials';
+import OrderForm from './OrderForm';
 
 
 // Very simple layout for now; Eventually need to set up navigation
 function App() {
   return (
-    <Specials />
-
+    <div>
+    
+      <OrderForm />
+    </div>
   );
 }
 
